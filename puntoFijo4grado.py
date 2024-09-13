86e331855836956aa152d940b9c1b4bf3af1c661
Hector Paulo <adrian.paulo.vaz@gmail.com>
2024-09-12 20:04:51 -0600

    mas chambitas

--- puntoFijo4grado.py
+++ puntoFijo4grado.py
@@ -1,6 +1,13 @@
 import numpy as np
+entradaa = 50053043044000159433307046053.0
 
-coeficientes = [1, 0, -80.5, 132, 374.0625]
+entrada = entradaa * 2
+lenEntrada = 30
+a = 1
+b = -80.5
+c = entrada
+discriminante = (b**2)+(((-1)*(b)+(4*a*c))**(1/2))
+coeficientes = [a, b, discriminante]
 soluciones = np.roots(coeficientes)
-print(f"Raíces (incluyendo complejas): {soluciones}")
-
+print(discriminante
+      )
